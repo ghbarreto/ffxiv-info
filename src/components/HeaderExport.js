5b14f4e82a667ed4cb6a50d6d4ccb58362ced51e
Gabriel <gh.barreto@hotmail.com>
2021-07-31 19:17:43 -0300

    added free company

--- src/components/HeaderExport.js
+++ src/components/HeaderExport.js
@@ -1,45 +1,54 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import {Menu} from 'semantic-ui-react';
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const HeaderExport = () => {
-  const [activeItem, setActiveItem] = useState('');
+    const [activeItem, setActiveItem] = useState('');
 
-  const handleItemClick = (e, { name }) => {
-    setActiveItem(name);
-  };
-  return (
-    <Menu pointing style={{ backgroundColor: 'rgb(241, 182, 18)' }}>
-      <Link to="/">
-        <Menu.Item
-          style={{ color: 'white', outline: 'black' }}
-          name="home"
-          active={activeItem === 'home'}
-          content="Home"
-          onClick={handleItemClick}
-        />
-      </Link>
-      <Link to="/characters">
-        <Menu.Item
-          style={{ color: 'white', outline: 'black' }}
-          name="messages"
-          active={activeItem === 'messages'}
-          content="Character Search"
-          onClick={handleItemClick}
-        />
-      </Link>
-      <Link to="/marketboard">
-        <Menu.Item
-          style={{ color: 'white', outline: 'black' }}
-          name="Marketboard"
-          active={activeItem === 'Marketboard'}
-          content="Marketboard"
-          onClick={handleItemClick}
-        />
-      </Link>
-    </Menu>
-  );
+    const handleItemClick = (e, {name}) => {
+        setActiveItem(name);
+    };
+    return (
+        <Menu pointing style={{backgroundColor: 'rgb(241, 182, 18)'}}>
+            <Link to="/">
+                <Menu.Item
+                    style={{color: 'white', outline: 'black'}}
+                    name="home"
+                    active={activeItem === 'home'}
+                    content="Home"
+                    onClick={handleItemClick}
+                />
+            </Link>
+            <Link to="/characters">
+                <Menu.Item
+                    style={{color: 'white', outline: 'black'}}
+                    name="messages"
+                    active={activeItem === 'messages'}
+                    content="Character Search"
+                    onClick={handleItemClick}
+                />
+            </Link>
+            <Link to="/marketboard">
+                <Menu.Item
+                    style={{color: 'white', outline: 'black'}}
+                    name="Marketboard"
+                    active={activeItem === 'Marketboard'}
+                    content="Marketboard"
+                    onClick={handleItemClick}
+                />
+            </Link>
+            <Link to="/free-company">
+                <Menu.Item
+                    style={{color: 'white', outline: 'black'}}
+                    name="Free Company"
+                    active={activeItem === 'Free Company'}
+                    content="Free Company"
+                    onClick={handleItemClick}
+                />
+            </Link>
+        </Menu>
+    );
 };
 
 export default HeaderExport;
