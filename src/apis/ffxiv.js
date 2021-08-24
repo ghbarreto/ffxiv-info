36d0324d7966401b34875fb735ccdcb6244ffca0
Gabriel <gh.barreto@hotmail.com>
2021-08-24 16:56:14 -0300

    fixed keys

--- src/apis/ffxiv.js
+++ src/apis/ffxiv.js
@@ -4,5 +4,8 @@ const KEY = 'f9260c55683c42c1966132a0cb28f757fd9ebb8614494792a6eaa944a4110b34';
 
 export default axios.create({
   baseURL: 'https://xivapi.com',
-  private_key: KEY,
+  params: {
+
+    private_key: KEY,
+  }
 });
