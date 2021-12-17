9fba96eb33ebe9a1befeb7d65aae316603b6665f
ivlen <gh.barreto@hotmail.com>
2021-12-17 18:29:08 -0300

    a

--- src/components/HeaderExport.js
+++ src/components/HeaderExport.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Menu} from 'semantic-ui-react';
-import {useState} from 'react';
-import {Link} from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const HeaderExport = () => {
     const [activeItem, setActiveItem] = useState('');
